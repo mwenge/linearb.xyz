43ef0d54296ae577f52cc1a6ca11f191aa5e7c42
Robert Hogan <robhogan@gmail.com>
2020-09-13 14:21:12 +0100

    Enable PWA

--- explorer.js
+++ explorer.js
@@ -2096,3 +2096,15 @@ window.onload = function() {
     zoomItem(inscription);
   }
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
