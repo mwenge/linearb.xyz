1b9cec37a5f521cfc06747a5eae1c30422be3094
Robert Hogan <robhogan@gmail.com>
2025-09-02 19:08:24 +0100

    Make the imagemap helper transcription draggable

--- imagemapper/summerHTMLImageMapCreator.js
+++ imagemapper/summerHTMLImageMapCreator.js
@@ -6,6 +6,31 @@
  * Released under the MIT license
  */
 
+function drag_start(event) {
+  var style = window.getComputedStyle(event.target, null);
+  event.dataTransfer.setData("text/plain",
+    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+} 
+function drop(event) {
+  var offset = event.dataTransfer.getData("text/plain").split(',');
+  var dm = document.getElementById('detail_panel');
+  dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+  dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+  event.preventDefault();
+  return false;
+}
+
+function drag_over(event) {
+  event.preventDefault();
+  return false;
+} 
+
+var dm = document.getElementById('detail_panel');
+dm.addEventListener('dragstart',drag_start,false);
+document.body.addEventListener('dragover',drag_over,false);
+document.body.addEventListener('drop',drop,false); 
+
+
 var summerHtmlImageMapCreator = (function() {
     'use strict';
     
