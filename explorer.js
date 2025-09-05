584ddfee0bb87b66895387bfd04fcf986e9f7a9a
Robert Hogan <robhogan@gmail.com>
2025-09-05 15:55:21 +0100

    Remove serviceworker

--- explorer.js
+++ explorer.js
@@ -1951,14 +1951,3 @@ window.onload = function() {
   }
 };
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
