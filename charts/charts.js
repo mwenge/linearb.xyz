89384879a06ad00b33d22dbb656be8c1e4e4a1fa
Robert Hogan <robhogan@gmail.com>
2025-09-09 20:11:32 +0100

    Resimplify word and letter charts

--- charts/charts.js
+++ charts/charts.js
@@ -28,112 +28,10 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-document.onkeydown = checkKey;
-function checkKey(e) {
-  if (e.defaultPrevented) {
-    return; // Do nothing if the event was already processed
-  }
-  switch(e.key) {
-    case "/": // '/' - focus search bar
-      toggleColor(document.getElementById("search-command"));
-      showSearch();
-      break;
-    case "c": // 'c' - clear search terms
-      var container = document.getElementById("search-terms");
-      container.innerHTML = "";
-      applySearchTerms();
-      break;
-    default:
-      return;
-  }
-  // Cancel the default action to avoid it being handled twice
-  e.preventDefault();
-}
-
-function showSearch() {
-  var search = document.getElementById('search');
-  var isVisible = search.style.visibility == "visible";
-  if (isVisible) {
-    search.style.visibility = 'hidden';
-    return;
-  }
-
-  var container = document.getElementById("chart-filter-details-container");
-  container.style.visibility = "hidden";
-
-  search.style.visibility = "visible";
-  search.focus();
-  search.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        updateSearch(event);
-      }
-  }); 
-}
-
-var highlightedSearchElements = [];
-function updateSearch(event) {
-  var searchTerm = event.target.value;
-  if (!searchTerm.length) {
-    return;
-  }
-  updateSearchTerms(event, searchTerm);
-}
-
-function updateSearchTerms(evt, searchTerm) {
-  var searchTerm = stripErased(searchTerm);
-  var container = document.getElementById("search-terms");
-  var existingElement = document.getElementById("search-for-" +  searchTerm);
-  if (existingElement) {
-    return;
-  }
-  var item = document.createElement("div");
-  item.className = 'search-term';
-  item.textContent = searchTerm;
-  item.id = "search-for-" + searchTerm;
-  item.setAttribute("term", searchTerm);
-  item.setAttribute("onclick", "removeFilter(event)");
-
-  var color = cycleColor();
-  item.style.backgroundColor = color;
-  item.style.opacity = "0.8";
-  item.setAttribute("highlightColor", color);
-
-  container.appendChild(item);
-  applySearchTerms();
-  evt.stopPropagation();
-}
-
-function removeFilter(evt) {
-  evt.target.parentElement.removeChild(evt.target);
-  applySearchTerms();
-}
 
 function lettersInWord(word) {
   var splitter = new GraphemeSplitter();
   return splitter.countGraphemes(word);
-}
-
-// Chart
-function shouldIncludeWord(word, type, tagsForWord) {
-  if (word == '—') {
-    return false;
-  }
-  if (tagsForWord.includes(type)) {
-    return true;
-  }
-  var splitter = new GraphemeSplitter();
-  var letters = splitter.countGraphemes(word);
-  if (type == "ideogram" && !tagsForWord.includes("number") && letters == 1) {
-    return true;
-  }
-  if (type == "word" && letters > 1 && !tagsForWord.includes("number")) {
-    return true;
-  }
-  if (type == "letter" && !tagsForWord.includes("number")) {
-    return true;
-  }
-  return false;
 }
 
 function isNumber(character) {
@@ -144,62 +42,10 @@ function isNumber(character) {
   return true;
 }
 
-function applySearchTerms() {
-  var searchTerms = document.getElementById("search-terms");
-  var numberOfSearchTerms = searchTerms.children.length;	
-  var searchTermValues = Array.prototype.slice.call(searchTerms.children)
-                         .map(x => stripErased(x.textContent));
-  var numberOfTags = activeTags.length;
-  var hasSearchTerm = (numberOfSearchTerms + numberOfTags > 0)
 
-  for (index in container.children) {
-    var element = container.children.item(index);
-    if (!hasSearchTerm) {
-      element.style.display = "block";
-      continue;
-    }
-    element.style.display = "none";
-    [activeTags, searchTermValues].flat(2).forEach( tag => {
-      if (element.name == tag) {
-        element.style.display = "block";
-      }
-    });
-  }
-}
-
-function toggleTag(event, tag) {
-  if (activeTags.includes(tag)) {
-    activeTags.splice(activeTags.indexOf(tag), 1);
-  } else {
-    activeTags.push(tag);
-  }
-  var element = event.target;
-  // Don't change the color of the tag if it is not being clicked from the menu
-  if (element.className == "charts-filter-tag") {
-    var color = element.style.backgroundColor;
-    element.style.color = color == tagColors[tag] ? "white" : "black";
-    element.style.backgroundColor = color == tagColors[tag] ? "black" : tagColors[tag];
-  }
-  applySearchTerms();
-}
-
-var tagColors = {};
-function toggleMetadatum(event, datum) {
-  if (!tagColors[datum]) {
-    tagColors[datum] = cycleColor(); 
-  }
-
-  toggleTag(event, datum);
-  event.stopPropagation();
-}
-
-function addLetterImagesToChart(img, image, inscription, container) {
+function addLetterImagesToChart(img, image, inscription) {
   return function (e) {
     if (!coordinates.has(image)) {
-      return;
-    }
-    // We're no longer displaying letters, abort.
-    if (container.type != "letter") {
       return;
     }
     var imageCoords = coordinates.get(image);
@@ -235,14 +81,6 @@ function addLetterImagesToChart(img, image, inscription, container) {
         label.className = "concordance-container-label";
         label.textContent = word + " in the Linear B corpus";
         d1.appendChild(label);
-
-        var filterItem = document.createElement("div");
-        filterItem.className = 'charts-filter-tag';
-        filterItem.textContent = word;
-        filterItem.setAttribute("onclick", "toggleMetadatum(event, '" + word + "')");
-        filterItem.style.backgroundColor = "black";
-        filterItem.style.color = "white";
-        filterDetailsContainer.appendChild(filterItem);
       }
       var d1 = document.createElement("div");
       d1.className = "inscription-container";
@@ -278,13 +116,9 @@ function addLetterImagesToChart(img, image, inscription, container) {
   };
 }
 
-function addWordImagesToChart(img, image, inscription, type, container) {
+function addWordImagesToChart(img, image, inscription, type) {
   return function (e) {
     if (!coordinates.has(image)) {
-      return;
-    }
-    // We're no longer displaying the type we loaded, abort.
-    if (container.type != type) {
       return;
     }
     var imageCoords = coordinates.get(image);
@@ -298,12 +132,7 @@ function addWordImagesToChart(img, image, inscription, type, container) {
       var area = imageCoords[i].coords;
       currentWord = wordIndexForLetterIndex(inscription.name, i);
 
-      var tagsForWord = inscription.wordTags[currentWord];
       var word = stripErased(inscription.words[currentWord]);
-      if (!shouldIncludeWord(word, type, tagsForWord)) {
-        prevWord = currentWord;
-        continue;
-      }
 
       if (currentWord != prevWord) {
         item = document.getElementById(type + "-" + word);
@@ -326,14 +155,6 @@ function addWordImagesToChart(img, image, inscription, type, container) {
           label.className = "concordance-container-label";
           label.textContent = word + " in the Linear A corpus";
           d1.appendChild(label);
-
-          var filterItem = document.createElement("div");
-          filterItem.className = 'charts-filter-tag';
-          filterItem.textContent = word;
-          filterItem.setAttribute("onclick", "toggleMetadatum(event, '" + word + "')");
-          filterItem.style.backgroundColor = "black";
-          filterItem.style.color = "white";
-          filterDetailsContainer.appendChild(filterItem);
         }
         var d1 = document.createElement("div");
         d1.className = "inscription-container";
@@ -363,119 +184,50 @@ function addWordImagesToChart(img, image, inscription, type, container) {
   };
 }
 
-function loadWords(inscription, type, container) {
-  var imagesToLoad = [];
-  if (loadFacsimiles) {
-    imagesToLoad = imagesToLoad.concat(inscription.facsimileImages);
-  }
-  if (loadPhotos) {
-    imagesToLoad = imagesToLoad.concat(inscription.images);
-  }
+function loadWords(inscription) {
+  var imagesToLoad = (imageType == "facsimile") ? inscription.facsimileImages : inscription.images;
   imagesToLoad.forEach( image => {
     var img = new Image();
     img.src = "../" + encodeURIComponent(image);
-    if (["word", "number", "ideogram"].includes(type)) {
-      img.addEventListener("load", addWordImagesToChart(img, image, inscription, type, container));
-    } else if (["letter"].includes(type)) {
-      img.addEventListener("load", addLetterImagesToChart(img, image, inscription, container));
+    if (displayType == "word") {
+      img.addEventListener("load", addWordImagesToChart(img, image, inscription));
+    } else {
+      img.addEventListener("load", addLetterImagesToChart(img, image, inscription));
     }
   });
 }
 
-function loadChart(evt, type) {
-
-  Array.prototype.map.call(document.getElementsByClassName("filters-container")[0]
-   .getElementsByClassName("filter-command"), x => x.style.backgroundColor = "black");
-  evt.target.style.backgroundColor = "purple";
-
-  if (container.type == type) {
-    filterDetailsContainer.style.visibility = filterDetailsContainer.style.visibility == "hidden" ? "visible" : "hidden";
-    var search = document.getElementById('search');
-    search.style.visibility = 'hidden';
-    return;
-  }
-
+function loadChart() {
   container.innerHTML = "";
-  filterDetailsContainer.innerHTML = "";
-  filterDetailsContainer.style.visibility = "hidden";
-  document.getElementById("search-terms").innerHTML = "";
-
-  container.type = type;
-  activeTags = [];
   for (var inscription of inscriptions.values()) {
-    loadWords(inscription, type, container);
+    loadWords(inscription);
   }
 }
 
-var loadPhotos = false;
-var loadFacsimiles = true;
-var togglePhotos = function(e) {
-    if (loadPhotos && !loadFacsimiles) {
-      return;
+var imageType = "facsimile";
+var displayType = "letter";
+var toggleImageType = function(e) {
+    if (imageType == "facsimile") {
+      imageType = "photo";
+      document.getElementById("facsimiles-command").style.backgroundColor = "black"; 
+      document.getElementById("photos-command").style.backgroundColor = "purple"; 
+    } else {
+      imageType = "facsimile";
+      document.getElementById("facsimiles-command").style.backgroundColor = "purple"; 
+      document.getElementById("photos-command").style.backgroundColor = "black"; 
     }
-    loadPhotos = !loadPhotos;
-    e.target.style.backgroundColor = e.target.style.backgroundColor == "black" ? "purple" : "black"; 
+    loadChart();
 };
-var toggleFacsimiles = function(e) {
-    if (loadFacsimiles && !loadPhotos) {
-      return;
+var toggleDisplayType = function(e) {
+    if (displayType == "letter") {
+      displayType = "word";
+      document.getElementById("words-command").style.backgroundColor = "purple"; 
+      document.getElementById("letters-command").style.backgroundColor = "black"; 
+    } else {
+      displayType = "letter";
+      document.getElementById("words-command").style.backgroundColor = "black"; 
+      document.getElementById("letters-command").style.backgroundColor = "purple"; 
     }
-    loadFacsimiles = !loadFacsimiles;
-    e.target.style.backgroundColor = e.target.style.backgroundColor == "black" ? "purple" : "black"; 
+    loadChart();
 };
 
-var filterDetailsContainer = null;
-var activeTags = [];
-function initializeChart() {
-  var supports = [];
-  var scribes = [];
-  var findspots = [];
-
-  loadInscriptionLevelTags();
-  //loadAnnotations();
-
-  filterDetailsContainer = document.getElementById("chart-filter-details-container");
-
-  container.type = "letter";
-  for (var inscription of inscriptions.values()) {
-    loadWords(inscription, "letter", container);
-  }
-
-  function loadInscriptionLevelTags() {
-    for (var inscription of inscriptions.values()) {
-      for (var item of [[supports, inscription.support],
-                        [scribes, inscription.scribe],
-                        [findspots, inscription.findspot],
-                        ]) {
-        var tag = item[1];
-        if (!tag) {
-          continue;
-        }
-        if (item[0].includes(tag)) {
-          continue;
-        }
-        item[0].push(tag);
-      }
-    }
-  }
-
-  var wordtags = [];
-  function loadAnnotations() {
-    var collectedWordTags = [];
-    for (var annotation of wordAnnotations) {
-      var inscription = inscriptions.get(annotation.name);
-      inscription.wordTags = [];
-      for (var word of annotation.tagsForWords) {
-        inscription.wordTags.push(word.tags);
-        collectedWordTags.push(...word.tags);
-      }
-    }
-
-    // Dedupe the list
-    wordtags = collectedWordTags.filter((v, i, a) => a.indexOf(v) === i);
-
-    // Filter out the site names
-    var sites = Array.from(inscriptions.values()).map(x => x.site).filter((v, i, a) => a.indexOf(v) === i);
-    wordtags = wordtags.filter((v, i, a) => !sites.includes(v));
-  }
-}
